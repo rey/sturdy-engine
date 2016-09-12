064666218c70eb44dc43cc73a533c82c175fbea9
Rey Dhuny <rey@me.com>
2016-09-12 06:38:02 +0100

    First!

---
+++ sturdy-engine.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# sturdy-engine.sh
+# Version 0.1
+# Rey Dhuny
+#
+#
+# ABOUT
+# =====
+#
+# Collect links, allow description, automagically create newsletter that can be
+# copy and pasted on a weekly basis
+
+
