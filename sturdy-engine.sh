5c7d1b02d71bb5fdad191e8f546a744a2a7ee227
Rey Dhuny <rey@me.com>
2016-09-12 07:13:09 +0100

    Add getOS, checkDependcies, createNewPost, checkLink

--- sturdy-engine.sh
+++ sturdy-engine.sh
@@ -12,3 +12,59 @@
 # copy and pasted on a weekly basis
 
 
+# pandoc -f markdown index.md > index.html
+
+# Get the OS flavour (eg. Linux or Darwin)
+function getOS() {
+  OS=`uname`
+}
+
+
+# Check the prerequisite software is installed
+function checkDependencies() {
+  which pandoc
+  # if which command was successful then
+  if [ $? -eq 0 ]; then
+    echo "pandoc is installed, proceeding"
+  else
+    echo "pandoc doesn't seem to be installed"
+    if [ ${OS} = "Darwin" ]; then
+      echo "Why not run \`brew install pandoc\`?"
+    else
+      echo "Why not run \`apt-get install pandoc\`?"
+    fi
+    exit
+  fi
+}
+
+function createNewPost() {
+  echo "Enter the link" && read LINK
+}
+
+function checkLink() {
+  # http://stackoverflow.com/a/3184819
+  regex="(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]"
+  if [[ ${LINK} =~ ${regex} ]]
+  then 
+      echo "Link valid"
+  else
+      echo "Link not valid"
+  fi
+}
+
+
+
+trap EXIT
+
+
+
+
+# getOS
+# checkDependencies
+createNewPost
+checkLink
+# enterDescription
+# getArchiveLink
+# convertToMarkdown
+# appendToNewsLetter
+
